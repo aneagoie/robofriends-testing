77184cdc7a44fbc2f38fa833ab0614230724ac70
aneagoie <aneagoie1@gmail.com>
2018-05-03 11:51:31 -0400

    circle ci4

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <h1 className='f1'>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends!</h1>
         <CounterButton />
       </div>
     );
