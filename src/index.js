8fad4cccbf094a74e57bc0bed0434ade98ef6853
shacharcohen <shaharcohen1997@gmail.com>
2019-01-18 10:50:45 +0200

    circle citest

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers';
 
 import './index.css';
-//aaa
 const logger = createLogger();
 
 const rootReducers = combineReducers({ requestRobots, searchRobots });
