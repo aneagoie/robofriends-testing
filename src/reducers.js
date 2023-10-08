956ff3f24503b9efb28275b7106871387de01516
Mohamad Mlaies <mo@mlaies.dev>
2023-10-08 17:12:04 +0200

    prettier test 3

--- src/reducers.js
+++ src/reducers.js
@@ -11,9 +11,9 @@ const initialStateSearch = {
 
 export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
-    case CHANGE_SEARCHFIELD:
-      return Object.assign({}, state, { searchField: action.payload });
-    default:
+           case CHANGE_SEARCHFIELD:
+                         return Object.assign({}, state, { searchField: action.payload });
+                            default:
       return state;
   }
 };
