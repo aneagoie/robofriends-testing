7d30a1a62f9452b8a37dc1e052cefd5cf5026fa2
Mohamad Mlaies <mo@mlaies.dev>
2023-10-08 16:48:21 +0200

    prettier

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setSearchField, requestRobots } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setSearchField, requestRobots } from "../actions";
 
-import MainPage from '../components/MainPage'
+import MainPage from "../components/MainPage";
 
-import './App.css';
-
+import "./App.css";
 
 const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending
-  }
-}
+    isPending: state.requestRobots.isPending,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
+    onRequestRobots: () => dispatch(requestRobots()),
+  };
+};
 
 class App extends Component {
   render() {
-    return <MainPage {...this.props}/>
+    return <MainPage {...this.props} />;
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import 'tachyons';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import "tachyons";
 
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import { requestRobots, searchRobots } from './reducers'
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { requestRobots, searchRobots } from "./reducers";
 
-import './index.css';
+import "./index.css";
 
-const logger = createLogger()
+const logger = createLogger();
 
-const rootReducers = combineReducers({requestRobots, searchRobots})
-const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+const store = createStore(
+  rootReducers,
+  applyMiddleware(thunkMiddleware, logger),
+);
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App/>
-  </Provider>
+    <App />
+  </Provider>,
 );
 
 // registerServiceWorker();
-
